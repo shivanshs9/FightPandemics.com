f106033f9cc04ccc5ce5c169cedd4901b8ad6920
Annie Giang <annieg4123@gmail.com>
2020-04-03 20:47:31 -0700

    Clean up code

--- client/src/components/Filter/FilterBox.js
+++ client/src/components/Filter/FilterBox.js
@@ -1,16 +1,15 @@
 import React from "react";
-import { Flex } from "antd-mobile";
 import styled from "styled-components";
+import { Flex } from "antd-mobile";
 import { FilterOption } from "./FilterOption";
 import { DARK_GRAY } from "../../constants/colors";
 
 export const FilterBox = () => {
   const labels = ["Location", "Provider", "Type", "Looking for"];
+  const titleStyles = { color: DARK_GRAY, fontWeight: "normal", margin: "0" };
   return (
     <div className="filter-box">
-      <h6 style={{ color: DARK_GRAY, fontWeight: "normal", margin: "0" }}>
-        Filter By
-      </h6>
+      <h6 style={titleStyles}>Filter By</h6>
       <Flex wrap="wrap">
         {labels.map((label, idx) => (
           <FilterOption key={idx} label={label} />
--- client/src/pages/Feed.js
+++ client/src/pages/Feed.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { Flex } from "antd-mobile";
 
 import { FilterBox } from "../components/Filter/FilterBox";
 import { Posts } from "../components/Posts/Posts";
